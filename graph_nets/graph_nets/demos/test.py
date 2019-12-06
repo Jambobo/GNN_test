c727e475147339c302503d9da5bf4fde2da4d81c
Jambobo <1065259087@qq.com>
2019-12-06 12:52:03 +0800

    Add files via upload

---
+++ graph_nets/graph_nets/demos/test.py
@@ -0,0 +1,56 @@
+#-*-coding:utf-8-*-
+
+import numpy as np
+import graph_nets.demos.graph_utils as gu
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+
+# #@title Visualize example graphs  { form-width: "30%" }
+# seed = 1  #@param{type: 'integer'}
+# rand = np.random.RandomState(seed=seed)
+#
+# num_examples = 15  #@param{type: 'integer'}
+# # Large values (1000+) make trees. Try 20-60 for good non-trees.
+# theta = 20  #@param{type: 'integer'}
+# num_nodes_min_max = (16, 17)
+#
+# input_graphs, target_graphs, graphs = gu.generate_networkx_graphs(
+#     rand, num_examples, num_nodes_min_max, theta)
+
+# num = min(num_examples, 16)
+# w = 3
+# h = int(np.ceil(num / w))
+# fig = plt.figure(40, figsize=(w * 4, h * 4))
+# fig.clf()
+# for j, graph in enumerate(graphs):
+#   ax = fig.add_subplot(h, w, j + 1)
+#   pos = gu.get_node_dict(graph, "pos")
+#   plotter = gu.GraphPlotter(ax, graph, pos)
+#   plotter.draw_graph_with_solution()
+#
+# plt.show()
+
+
+def draw(g):                   #显示图
+
+    pos=nx.spring_layout(G)
+
+    nx.draw(g,pos,arrows=True,with_labels=True,nodelist=G.nodes(),style='dashed',edge_color='b',width=2,\
+
+          node_color='y',alpha=0.5)
+
+    plt.show()
+
+
+G = nx.DiGraph()
+
+G.add_path([1,2,3,4,5,6])
+
+draw(G)
+
+node_connected = nx.all_pairs_node_connectivity(G)
+
+print(node_connected)
+
